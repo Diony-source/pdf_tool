5efae79abc1f9addfda78cb4340a86d96ebdeab1
Diony-source <erendalkiran2001@gmail.com>
2024-12-25 22:33:20 +0300

    feat(services): Add service for merging PDF files

--- services/pdf_merge.go
+++ services/pdf_merge.go
@@ -2,22 +2,28 @@ package services
 
 import (
 	"fmt"
+	"pdf_tool/utils"
+
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
-	"os"
 )
 
 // MergePDFs merges multiple PDF files into one
 func MergePDFs(inputFiles []string, outputFile string) error {
-	// Check if the output file already exists
-	if _, err := os.Stat(outputFile); err == nil {
-		return fmt.Errorf("output file already exists")
+	var fullPaths []string
+
+	// Find the full paths of the input files
+	for _, file := range inputFiles {
+		fullPath, err := utils.FindFileInSystem("C:\\", file) // Search from C:\ (root directory)
+		if err != nil {
+			return fmt.Errorf("file %s not found: %v", file, err)
+		}
+		fullPaths = append(fullPaths, fullPath)
 	}
 
 	// Create a default PDF configuration
 	conf := model.NewDefaultConfiguration()
 
 	// Merge PDF files
-	// The third argument is 'true' to overwrite the output file if it exists
-	return api.MergeCreateFile(inputFiles, outputFile, true, conf)
+	return api.MergeCreateFile(fullPaths, outputFile, true, conf)
 }
